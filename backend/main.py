2905b36a5f004089a49c42e6031afd2c7390224e
hydraadra112 <johnmanuel.carado@wvsu.edu.ph>
2025-04-27 01:28:01 +0800

    chore: explored parsing techniques

--- backend/main.py
+++ backend/main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from joblib import load
+from src.preprocessors import load_stopwords
+import calamancy # load in states in future due to slow loading time
 
 app = FastAPI()
 
@@ -11,6 +14,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+@app.on_event("startup")
+def load_model():
+    app.state.model = load("./src/notebooks/serialized/models/RandomForestClassifier.pkl") # temporarily using RF
+    app.state.calamancy = calamancy.load("tl_calamancy_md-0.2.0")
+    app.state.stopwords = load_stopwords()
 
 @app.get("/api/hello")
 async def hello():
