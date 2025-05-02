de37df1bc0d1ff4e16c56c0724539c24c1eaa09a
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2025-05-02 19:12:01 +0800

    update: modified tailwindconfig and svelte congig to allow postCSS to enable the styling variables. NOW WORKING

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
-// tailwind.config.js
 module.exports = {
-    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx,.svelte}'],
     theme: {
       extend: {
         fontFamily: {
