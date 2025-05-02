608261a94a8e5ffb6f2e4fbbdf0ca280d72a6cc9
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2025-05-02 19:01:19 +0800

    update: added tailwind config files for styling variables usage

--- frontend/src/routes/components/Header.svelte
+++ frontend/src/routes/components/Header.svelte
@@ -1,12 +1,12 @@
 <script>
-
 </script>
 
 <header class="flex flex-row gap-x-5">
-	<p>Hello from Header! Click this ---> </p>
+	<p>Hello from Header! Click this ---></p>
 	<a href="/about">About Page</a>
 	<a href="/sample">Backend</a>
 	<a href="/">Home</a>
+	<a href="/analyze">Analyze</a>
 </header>
 
 <style>
