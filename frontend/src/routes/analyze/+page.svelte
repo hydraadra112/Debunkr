73e76bde534d1fa5f2a9c0852866458a36103371
Mark Andrei Encanto <markandrei.encanto@wvsu.edu.ph>
2025-05-12 16:47:58 +0800

    feat: add condition to  not show analysis results before the processing is finished and removed word scores in the influential words results

--- frontend/src/routes/analyze/+page.svelte
+++ frontend/src/routes/analyze/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import { TextLinkPredict } from '$lib/api';
 	import { renderChart } from '$lib/chart';
-	/* SEND DATA TO BACKEND AND GET PREDS */
+	import { tick } from 'svelte';
+
 	type input = {
 		text: string;
 	};
@@ -19,8 +20,11 @@
 		try {
 			const payload: input = { text: inputText };
 			const res = await TextLinkPredict<response, input>('/api/predict-text-link', payload);
+
 			prediction = res.result;
 			wordScores = res.scores;
+
+			await tick();
 
 			renderChart(wordScores);
 		} catch (err) {
@@ -28,11 +32,10 @@
 			alert('Failed to get prediction.');
 		}
 	}
-	/* END */
 </script>
 
 <div>
-	<div class="mt-10 flex h-[130vh] w-full flex-col items-center justify-center p-8">
+	<div class="mt-10 flex h-[110vh] flex-col items-center justify-center overflow-hidden p-8">
 		<p class=" font-heading primary-color my-4 text-[2rem]">Classify News for Accuracy</p>
 		<div
 			class="border-c3 z-3 col-2 flex h-full w-[90%] rounded-[2rem] border-1 bg-[#8B7358]/5 shadow-xl backdrop-blur-lg"
@@ -79,59 +82,70 @@
 
 			<!-- Results Section -->
 			<section class="flex w-full flex-col rounded-lg px-6 pt-6">
-				<h1 class="font-heading primary-color mb-4 self-center text-2xl">Analysis Result</h1>
-				<div class="h-full space-y-6">
-					<div id="results-container">
-						{#if prediction}
+				{#if !prediction}
+					<h1 class="font-heading primary-color mb-4 self-center text-2xl">Analysis Result</h1>
+					<p class="mt-6 flex justify-center text-[#6b7280] italic">
+						The analysis results will appear here.
+					</p>
+				{:else}
+					<h1 class="font-heading primary-color mb-4 self-center text-2xl">Analysis Result</h1>
+
+					<div class="h-full space-y-6">
+						<div id="results-container">
 							<h3 class="font-body primary-color my-2 text-xl">The News is Likely {prediction}</h3>
-						{:else}
-							<h3 class="font-body primary-color my-2 text-xl">The News is Likely ???</h3>
-						{/if}
 
-						<div id="emotions-list" class="flex flex-wrap gap-3">
-							<!-- Answers will appear here -->
-							<p class="desc-color italic">Analysis Description</p>
-						</div>
-					</div>
-					<div id="confidence-container">
-						<h3 class="font-body primary-color mb-2 text-xl">Confidence Level</h3>
-						<div id="confidence-list" class="flex flex-wrap gap-3">
-							<!-- Answers will appear here -->
-							<p class="desc-color italic">Confidence Level Description</p>
-						</div>
-					</div>
-					<div id="response-container">
-						<h3 class="font-body primary-color mb-2 text-xl">Visualizations</h3>
-
-						<div class="flex gap-4">
-							<!-- Word list -->
-							<div id="visualizations-response" class="w-1/2 overflow-auto">
-								{#if Object.keys(wordScores).length > 0}
-									<ul class="list-disc pl-5">
-										{#each Object.entries(wordScores) as [word, score]}
-											<li><strong>{word}</strong>: {score}</li>
-										{/each}
-									</ul>
-								{:else}
-									<p class="desc-color italic">
-										The influential words and the graph will be shown here.
-									</p>
-								{/if}
-							</div>
-							<!--wordchart-->
-							<div class="w-1/2">
-								<canvas id="wordChart" width="400" height="300"></canvas>
+							<div id="emotions-list" class="flex flex-wrap gap-3">
+								<!-- Answers will appear here -->
+								<p class="desc-color italic">Analysis Description</p>
 							</div>
 						</div>
+
+						<div id="confidence-container">
+							<h3 class="font-body primary-color mb-2 text-xl">Confidence Level</h3>
+							<div id="confidence-list" class="flex flex-wrap gap-3">
+								<p class="desc-color italic">Confidence Level Description</p>
+							</div>
+						</div>
+
+						<div id="response-container">
+							<h3 class="font-body primary-color mb-2 text-xl">Influential Words</h3>
+
+							<div class="flex gap-4">
+								<!-- Word list -->
+								<div id="visualizations-response" class="w-1/2 overflow-auto">
+									{#if Object.keys(wordScores).length > 0}
+										<ul class="list-disc pl-5">
+											{#each Object.entries(wordScores) as [word, score]}
+												<li>
+													<strong style="color: {score >= 0 ? '#4D7C5A' : '#C84A3D'}">
+														{word}
+													</strong>
+												</li>
+											{/each}
+										</ul>
+									{:else}
+										<p class="desc-color italic">
+											The influential words and the graph will be shown here.
+										</p>
+									{/if}
+								</div>
+
+								<!-- wordchart -->
+								<div class="w-1/2">
+									<canvas id="wordChart" width="400" height="300"></canvas>
+								</div>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div class="bottom-0 flex items-center justify-end gap-2 self-end py-6">
-					<section class="font-heading">Status:</section>
-					<section class="h-3 w-3 rounded-full bg-[#C84A3D]"></section>
-					<section class="font-body">Likely Fake</section>
-					<section class="h-3 w-3 rounded-full bg-[#4D7C5A]"></section>
-					<section class="font-body">Likely Real</section>
-				</div>
+
+					<div class="bottom-0 flex items-center justify-end gap-2 self-end py-6">
+						<section class="font-heading">Status:</section>
+						<section class="h-3 w-3 rounded-full bg-[#C84A3D]"></section>
+						<section class="font-body">Likely Fake</section>
+						<section class="h-3 w-3 rounded-full bg-[#4D7C5A]"></section>
+						<section class="font-body">Likely Real</section>
+					</div>
+				{/if}
 			</section>
 		</div>
 		<div class="mt-2 ml-[22%] flex w-full justify-start">
